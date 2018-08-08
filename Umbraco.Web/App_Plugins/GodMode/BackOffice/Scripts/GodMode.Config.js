0e5248fbb35e5641ccc380fdd5bddc4df2df9ba4
DanDiplo <dan@diplo.co.uk>
2018-08-08 13:02:33 +0100

    Updated version to 1.5.1

--- Umbraco.Web/App_Plugins/GodMode/BackOffice/Scripts/GodMode.Config.js
+++ Umbraco.Web/App_Plugins/GodMode/BackOffice/Scripts/GodMode.Config.js
@@ -4,7 +4,7 @@ angular.module("umbraco")
     .constant("godModeConfig", {
         "baseApiUrl": "BackOffice/Api/GodModeApi/",
         "config": {
-            "version": "1.5.0",
+            "version": "1.5.1",
             "editTemplateUrl": "#/settings/templates/edit/",
             "editDataTypeUrl": "#/developer/datatypes/edit/",
             "editDocTypeUrl": "#/settings/documentTypes/edit/",
