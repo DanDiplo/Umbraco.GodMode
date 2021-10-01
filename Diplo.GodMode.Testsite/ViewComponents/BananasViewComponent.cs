a6336ad574932b02b63d47c7640c9504a70da9e1
Dan Booth <dan@diplo.co.uk>
2021-10-01 21:14:44 +0100

    More test async partials and view components

---
+++ Diplo.GodMode.Testsite/ViewComponents/BananasViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Extensions;
+
+namespace Diplo.GodMode.Testsite.ViewComponents
+{
+    [ViewComponent(Name = "BananasList")]
+    public class BananasViewComponent : ViewComponent
+    {
+        private readonly IPublishedContentQuery publishedContentQuery;
+
+        public BananasViewComponent(IPublishedContentQuery publishedContentQuery)
+        {
+            this.publishedContentQuery = publishedContentQuery;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int maxItems = 3)
+        {
+            var items = GetRootContent().Take(maxItems);
+            return View(items);
+        }
+
+        public IEnumerable<IPublishedContent> GetRootContent()
+        {
+            return publishedContentQuery.ContentAtRoot().SelectMany(x => x.Children());
+        }
+    }
+}
---
+++ Diplo.GodMode.Testsite/ViewComponents/RootContentViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Extensions;
+
+namespace Diplo.GodMode.Testsite.ViewComponents
+{
+    [ViewComponent(Name = "RootContentList")]
+    public class RootContentViewComponent : ViewComponent
+    {
+        private readonly IPublishedContentQuery publishedContentQuery;
+
+        public RootContentViewComponent(IPublishedContentQuery publishedContentQuery)
+        {
+            this.publishedContentQuery = publishedContentQuery;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int maxItems = 3)
+        {
+            var items = GetRootContent().Take(maxItems);
+            return View(items);
+        }
+
+        public IEnumerable<IPublishedContent> GetRootContent()
+        {
+            return publishedContentQuery.ContentAtRoot().SelectMany(x => x.Children());
+        }
+    }
+}
