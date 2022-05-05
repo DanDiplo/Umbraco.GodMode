52363ead44458ff64c0b15cb46c8e4ce683d6c44
Dan Booth <dan@diplo.co.uk>
2022-05-05 17:23:55 +0100

    Just for testing compositions

---
+++ Diplo.GodMode.Testsite/TestComposer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Diplo.GodMode.Testsite
+{
+    public class TestComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.Services.AddScoped<IDiploFoo, DiploFoo>();
+        }
+    }
+
+    public interface IDiploFoo
+    {
+        string Foo();
+    }
+
+    public class DiploFoo : IDiploFoo
+    {
+        string IDiploFoo.Foo()
+        {
+            return "Diplo Foo to You!";
+        }
+    }
+}
